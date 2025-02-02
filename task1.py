fba93a393c10a23fc268bb8ba694709e2a10ed63
Abdullah Ghallab <113211590+ghallababdullah@users.noreply.github.com>
2025-02-02 23:40:17 +0300

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,68 +1,18 @@
-class Book:
-    """ Базовый класс книги. """
-    def __init__(self, name: str, author: str):
-        self.__name = name
-        self.__author = author
-    @property
-    def name(self):
-        return self.__name
+SALARY = 5000  # Ежемесячная зарплата
+SPEND = 6000  # Траты за первый месяц
+MONTHS = 10  # Количество месяцев, которое планируется протянуть без долгов
+INCREASE = 0.03  # Ежемесячный рост цен
 
-    @property
-    def author(self):
-        return self.__author
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+money_capital = 0
+current_spend = SPEND
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+for month in range(MONTHS):
+    if month > 0:  # Увеличение расходов на 3% с второго месяца
+        current_spend *= (1 + INCREASE)
+    # Расчет необходимой суммы из подушки безопасности
+    needed_from_capital = current_spend - SALARY
+    # Обновление подушки безопасности
+    money_capital += needed_from_capital
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
-
-
-class PaperBook(Book):
-    def __init__(self, name: str, author: str, pages: int):
-        super().__init__(name,author)
-        self.pages = pages
-
-    @property
-    def pages(self):
-        return self._pages
-
-    @pages.setter
-    def pages(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Количество страниц должно быть целым числом")
-        if value <= 0:
-            raise ValueError("Количество страниц должно быть положительным числом")
-        self._pages = value
-        
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
-
-
-class AudioBook(Book):
-    def __init__(self, name: str, author: str, duration: float):
-        super().__init__(name, author)
-        self.duration = duration
-
-    @property
-    def duration(self):
-        return self._duration
-
-    @duration.setter
-    def duration(self, value):
-        if not isinstance(value, (float, int)):
-            raise TypeError("Продолжительность должна быть числом")
-        if value <= 0:
-            raise ValueError("Продолжительность должна быть положительным числом")
-        self._duration = float(value)
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
-
-# Примеры работы с классами
-book1 = PaperBook("Война и мир", "Лев Толстой", 1225)
-book2 = AudioBook("1984", "Джордж Оруэлл", 12.5)
-
-print(book1)
-print(book2)
-print(repr(book1))
-print(repr(book2))
+print(f"Подушка безопасности, чтобы протянуть {MONTHS} месяцев без долгов:", int(money_capital))
